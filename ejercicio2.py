1cd176ecd20c963849c0928edb2c8ba24fe4d309
christian <cristian@ldgsoluciones.com>
2020-01-09 09:12:52 -0500

    second commit

--- ejercicio2.py
+++ ejercicio2.py
@@ -10,38 +10,42 @@ def manejoEquipaje():
     pesoPromedio = 0
     while numeroBultos > 0:
         pesoBulto = int(input("Ingrese el peso de cada Bulto :"))
-        if pesoBulto < 500:           
-            tablaBultos.append(pesoBulto) 
-                # tablaPrecioKilo.append(precioKilo)                                
+        if pesoBulto < 500:
+            tablaBultos.append(pesoBulto)
+            # tablaPrecioKilo.append(precioKilo)
         else:
             print('El peso del bulto excede el peso requerido')
 
-        numeroBultos-=1
+        numeroBultos -= 1
     for i in tablaBultos:
         if i <= 25:
             precioKilo = 0
-        elif i  > 25 and i < 300:
+        elif i > 25 and i < 300:
             precioKilo = 1500
         else:
-            precioKilo = 2500    
-        tablaPrecioKilo.append(precioKilo)     
+            precioKilo = 2500
+        tablaPrecioKilo.append(precioKilo)
 
     for j in tablaBultos:
         if j > bultoPesado:
             bultoPesado = j
         elif j < bultoLiviano:
             bultoLiviano = j
-        pesoPromedio+=j / len(tablaBultos)    
+        pesoPromedio += j / len(tablaBultos)
 
-
-    return ["Peso", 
-            tablaBultos,[            
-            len(tablaBultos),
-            bultoLiviano,
-            bultoPesado, 
-            pesoPromedio],
-            tablaPrecioKilo            
+    return ["Valores Peso Tabla : ",
+            tablaBultos,
+            "Valores precio : ",
+            tablaPrecioKilo,
+            ["Numero de bultos totales ",
+             len(tablaBultos),
+             "Bulto menor peso ",
+             bultoLiviano,
+             "Bulto mayor peso ",
+             bultoPesado,
+             "Peso promedio "
+             pesoPromedio]
             ]
-    
+
 
 print(manejoEquipaje())
