7057cca7ba28fdcd0df70482243f74b03ee82a00
christian <cristian@ldgsoluciones.com>
2020-01-09 09:02:55 -0500

    first commit

---
+++ ejercicio1.js
@@ -0,0 +1,15 @@
+function calcularPasaje(distancia, estancia) {
+  let precioPasaje = 0;
+  let precioKm = 35;
+  let precioDescuento = 0;
+
+  if (distancia > 1000 && estancia > 7) {
+    precioPasaje = precioKm * distancia;
+    precioDescuento = precioPasaje - precioPasaje * 0.3;
+    return precioDescuento;
+  } else {
+    precioPasaje = precioKm * distancia;
+    return precioPasaje;
+  }
+}
+console.log(calcularPasaje(1100, 8));
---
+++ ejercicio2.py
@@ -0,0 +1,47 @@
+
+def manejoEquipaje():
+    numeroBultos = int(input("Por favor ingrese el número de bultos :"))
+    tablaBultos = []
+    precioKilo = 0
+    tablaPrecioKilo = []
+    totalBultos = 0
+    bultoPesado = 0
+    bultoLiviano = 500
+    pesoPromedio = 0
+    while numeroBultos > 0:
+        pesoBulto = int(input("Ingrese el peso de cada Bulto :"))
+        if pesoBulto < 500:           
+            tablaBultos.append(pesoBulto) 
+                # tablaPrecioKilo.append(precioKilo)                                
+        else:
+            print('El peso del bulto excede el peso requerido')
+
+        numeroBultos-=1
+    for i in tablaBultos:
+        if i <= 25:
+            precioKilo = 0
+        elif i  > 25 and i < 300:
+            precioKilo = 1500
+        else:
+            precioKilo = 2500    
+        tablaPrecioKilo.append(precioKilo)     
+
+    for j in tablaBultos:
+        if j > bultoPesado:
+            bultoPesado = j
+        elif j < bultoLiviano:
+            bultoLiviano = j
+        pesoPromedio+=j / len(tablaBultos)    
+
+
+    return ["Peso", 
+            tablaBultos,[            
+            len(tablaBultos),
+            bultoLiviano,
+            bultoPesado, 
+            pesoPromedio],
+            tablaPrecioKilo            
+            ]
+    
+
+print(manejoEquipaje())
